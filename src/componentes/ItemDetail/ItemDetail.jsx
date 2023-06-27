4ae250070fd588b5cb0f32a92616fcfccb96da53
ale <AROrtega@hotmail.com>
2023-06-27 15:38:46 -0300

    nuevo ItemDetail.css, estilos para los detalles de item, fix colores y diseño

--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,11 +1,15 @@
+import "./ItemDetail.css"
+import ItemCount from "../ItemCount/ItemCount"
+
 const ItemDetail = ({ id, nombre, precio, img }) => {
     return (
-        <div className='contenedorItem'>
-            <h2>Nombre: {nombre} </h2>
-            <h3>Precio: {precio} </h3>
-            <h3>ID: {id} </h3>
-            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga odio eveniet facere maiores quo tempore quisquam! Consectetur dolores quos ducimus maiores quam quae, eveniet voluptatibus beatae, nemo cumque tempore modi?</p>
-            <img src={img} alt={nombre} />
+        <div className='divDetail'>
+            <h2 className="h2Detail">{nombre} </h2>
+            <img className="imgDetail" src={img} alt={nombre}/>
+            <h3 className="h3Detail">Precio con Tarjeta: {precio}</h3>
+            <h4 className="h4Detail">Precio en Efectivo: {precio * 0.75}</h4>
+            <h5 className="h5Detail">ID Producto: {id}</h5>
+            <ItemCount stock={10} inicial={1}/>
         </div>
     )
 }
