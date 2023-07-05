abbcf8aac89a3c6d0f3768035f6f396034a3d2e0
ale <AROrtega@hotmail.com>
2023-07-05 15:41:21 -0300

    actividad numero 4 implementada, añadido stock a la base de datos, nuevas props en itemDetail

--- src/componentes/ItemCount/ItemCount.jsx
+++ src/componentes/ItemCount/ItemCount.jsx
@@ -1,27 +1,31 @@
-import { useState } from "react"
+import { Fragment, useState } from "react"
 import './ItemCount.css'
 
-const ItemCount = ({stock, inicial}) => {
+const ItemCount = ({ stock, inicial, funcionAgregar }) => {
     const [contador, setContador] = useState(inicial)
 
     const incrementarContador = () => {
-        if(contador < stock) {
+        if (contador < stock) {
             setContador(contador + 1);
         }
     }
 
     const disminuirContador = () => {
-        if(contador > inicial) {
+        if (contador > inicial) {
             setContador(contador - 1);
         }
     }
-  return (
-    <div className="flexContador">
-        <button className="botonDisminuir" onClick={disminuirContador}> - </button>
-        <p className="contadorEstilo"> {contador} </p>
-        <button className="botonIncrementar" onClick={incrementarContador}> + </button>
-    </div>
-  )
+
+    return (
+        <>
+            <div className="flexContador">
+                <button className="botonDisminuir" onClick={disminuirContador}> - </button>
+                <p className="contadorEstilo"> {contador} </p>
+                <button className="botonIncrementar" onClick={incrementarContador}> + </button>
+            </div>
+            <button onClick={ () => funcionAgregar(contador)}> Agregar al Carrito </button>
+        </>
+    )
 }
 
 export default ItemCount
--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,15 +1,26 @@
 import "./ItemDetail.css"
 import ItemCount from "../ItemCount/ItemCount"
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
-const ItemDetail = ({ id, nombre, precio, img }) => {
+const ItemDetail = ({ id, nombre, precio, img, stock }) => {
+    const [agregarCantidad, setAgregarCantidad] = useState(0);
+
+    const handlerCantidad = (cantidad) => {
+        setAgregarCantidad(cantidad)
+        console.log("Productos agregados:" + cantidad);
+    }
+
     return (
         <div className='divDetail'>
             <h2 className="h2Detail">{nombre} </h2>
             <img className="imgDetail" src={img} alt={nombre}/>
             <h3 className="h3Detail">Precio con Tarjeta: {precio}</h3>
-            <h4 className="h4Detail">Precio en Efectivo: {precio * 0.75}</h4>
-            <h5 className="h5Detail">ID Producto: {id}</h5>
-            <ItemCount stock={10} inicial={1}/>
+            <h5 className="h5Detail">ID Producto:{id}</h5>
+
+            {
+                agregarCantidad > 0 ? (<Link to={"/cart"}> Finalizar Compra</Link>) : (<ItemCount stock={stock} inicial={1} funcionAgregar={handlerCantidad}/>)
+            }
         </div>
     )
 }
