c0a6edfdc0961a6c4537ada879bd3d74e56b6eed
ale <AROrtega@hotmail.com>
2023-07-16 11:47:13 -0300

    creación de archivos .css y agregado de estilos en el Cart y CartItem

--- src/componentes/Cart/Cart.jsx
+++ src/componentes/Cart/Cart.jsx
@@ -2,6 +2,7 @@ import { Fragment, useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 import { Link } from "react-router-dom";
 import CartItem from "../CartItem/CartItem";
+import './Cart.css'
 
 const Cart = () => {
     const { cart, clearCart, total, totalAmount } = useContext(CartContext);
@@ -9,16 +10,17 @@ const Cart = () => {
     if (totalAmount === 0) {
         return (
             <>
-                <h2>Carrito Vacio, no tiene productos</h2>
-                <Link to="/">Seguir Comprando</Link>
+                <h2 className="h2CartEmpty">Tu carrito está vacio</h2>
+                <Link className="linkCart" to="/">Seguir Comprando</Link>
             </>
         )
     }
     return (
         <div>
+            <h2 className="h2Cart">Tu carrito de compra</h2>
             {cart.map(producto => <CartItem key={producto.id} {...producto} />)}
-            <h2>Total: ${total}</h2>
-            <h3>Cantidad Total: {totalAmount}</h3>
+            <h3>Total: ${total}</h3>
+            <h4>Cantidad Total: {totalAmount}</h4>
             <button onClick={() => clearCart()}>Vaciar Carrito</button>
             <Link to="/checkout">Finalizar Compra</Link>
         </div>
--- src/componentes/CartItem/CartItem.jsx
+++ src/componentes/CartItem/CartItem.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
+import "./CartItem.css"
 
 const CartItem = ({item, cantidad}) => {
     const {deleteProduct} = useContext(CartContext)
@@ -9,7 +10,7 @@ const CartItem = ({item, cantidad}) => {
         <h3>{item.nombre}</h3>
         <p>Cantidad: {cantidad}</p>
         <p>Precio: ${item.precio}</p>
-        <button onClick={() => deleteProduct(item.id)}> Eliminar </button>
+        <button className="btnCartItem" onClick={() => deleteProduct(item.id)}> Eliminar </button>
         <hr/>
     </div>
   )
