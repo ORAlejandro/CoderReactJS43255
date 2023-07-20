f6cc3801272ce0ae89531bcc18ff7f2de891e74f
ale <AROrtega@hotmail.com>
2023-07-19 23:51:36 -0300

    test

--- src/componentes/CartItem/CartItem.jsx
+++ src/componentes/CartItem/CartItem.jsx
@@ -7,7 +7,7 @@ const CartItem = ({item, cantidad}) => {
 
   return (
     <div>
-        <h3>{item.nombre}</h3>
+        <h3>Producto: {item.nombre}</h3>
         <p>Cantidad: {cantidad}</p>
         <p>Precio: ${item.precio}</p>
         <button className="btnCartItem" onClick={() => deleteProduct(item.id)}> Eliminar </button>
