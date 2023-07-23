708a274349b5c7a3b5ad183ea4ba27f442eb6f9d
ale <AROrtega@hotmail.com>
2023-07-23 12:25:36 -0300

    estilos para checkout e item, nuevo boton 'volver a comprar' luego de finalizar una orden

--- src/componentes/Checkout/Checkout.jsx
+++ src/componentes/Checkout/Checkout.jsx
@@ -3,6 +3,7 @@ import { CartContext } from "../../context/CartContext";
 import { db } from "../../services/config";
 import { collection, addDoc, updateDoc, doc, getDoc } from "firebase/firestore";
 import './Checkout.css'
+import { Link } from "react-router-dom";
 
 const Checkout = () => {
     const { cart, clearCart, totalAmount, total } = useContext(CartContext);
@@ -45,7 +46,7 @@ const Checkout = () => {
             orden.items.map(async (productoOrden) => {
                 const productoRef = doc(db, "inventario", productoOrden.id);
                 const productoDoc = await getDoc(productoRef);
-                const currentlyStock = productoDoc.data().stock; 
+                const currentlyStock = productoDoc.data().stock;
 
                 await updateDoc(productoRef, {
                     stock: currentlyStock - productoOrden.cantidad
@@ -73,49 +74,62 @@ const Checkout = () => {
 
     return (
         <div>
-            <h2>CHECKOUT</h2>
+            <h2 className="styleCheckout">Resumen de tu Compra</h2>
+            <hr />
             <form onSubmit={handlerForm}>
-                {cart.map(producto => (
-                    <div key={producto.id}>
-                        <p>
-                            {producto.item.nombre} x {producto.cantidad}
-                        </p>
-                        <p>Precio $ {producto.item.precio} </p>
-                        <hr />
-                    </div>
-                ))}
-                <p>Total: ${total}</p>
+                <div className="flexResumen">
+                    {cart.map(producto => (
+                        <div key={producto.id} className="contenedorProducto">
+                            <p>Producto: <span className="spanBold">{producto.item.nombre}</span></p>
+                            <p>Cantidad: <span className="spanBold">{producto.cantidad}</span></p>
+                            <p><span className="spanBold">${producto.item.precio}</span></p>
+                        </div>
+                    ))}
+                    <p className="spanBold styleTotal">Total: ${total}</p>
+                </div>
+                <hr />
+
+                <h3 className="styleCheckout">¡Tu compra está casi lista!</h3>
                 <hr />
 
                 <div className="styleForm">
-                    <label htmlFor="nombre">Nombre</label>
-                    <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+                    <label className="styleLabel" htmlFor="nombre">Nombre</label>
+                    <input className="styleInput" type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
 
-                    <label htmlFor="apellido">Apellido</label>
-                    <input type="text" value={apellido} onChange={(e) => setApellido(e.target.value)} />
+                    <label className="styleLabel" htmlFor="apellido">Apellido</label>
+                    <input className="styleInput" type="text" value={apellido} onChange={(e) => setApellido(e.target.value)} />
 
-                    <label htmlFor="telefono">Telefono</label>
-                    <input type="text" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
+                    <label className="styleLabel" htmlFor="telefono">Telefono</label>
+                    <input className="styleInput" type="text" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
 
-                    <label htmlFor="email">Email</label>
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <label className="styleLabel" htmlFor="email">Email</label>
+                    <input className="styleInput" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
 
-                    <label htmlFor="emailConfirmacion">Confirmación Email</label>
-                    <input type="email" value={confirmEmail} onChange={(e) => setConfirmEmail(e.target.value)} />
+                    <label className="styleLabel" htmlFor="emailConfirmacion">Confirmación Email</label>
+                    <input className="styleInput" type="email" value={confirmEmail} onChange={(e) => setConfirmEmail(e.target.value)} />
                 </div>
 
                 {
                     error && <p> {error} </p>
                 }
 
-            <div className="flexEnviar">
-                <button className="btnEnviar" type="submit">Enviar</button>
+                <div className="flexEnviar">
+                    <button className="btnEnviar" type="submit">Enviar</button>
                 </div>
+                <hr />
 
             </form>
             {
                 orderId && (
-                    <p className="styleOrden">Compra con éxito, tu número de orden es {orderId}</p>
+                    <div>
+                        <p className="styleOrden">¡Compra finalizada! Gracias por elegirnos. Tu número de orden es {orderId}</p>
+                        <hr />
+                        <Link className="linkVolver" to="/">
+                            <div className="flexBtnVolver">
+                                <button className="btnVolverAComprar"> Volver a Comprar </button>
+                            </div>
+                        </Link>
+                    </div>
                 )
             }
         </div>
